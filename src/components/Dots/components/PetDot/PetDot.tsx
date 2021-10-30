986c1d69407f25dc067b930c87fc380163cae631
StrykeDev <attias.barak@gmail.com>
2021-10-30 15:15:59 +0300

    Improve pet dot preview

--- src/components/Dots/components/PetDot/PetDot.tsx
+++ src/components/Dots/components/PetDot/PetDot.tsx
@@ -1,35 +1,40 @@
 import React, { useState } from 'react';
 import Dot, { EDotSize, IDots } from '../../Dot';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import {
+   faAngry,
    faAppleAlt,
    faBacon,
    faBed,
    faBolt,
+   faBox,
    faCarrot,
-   faCookie,
+   faCoffee,
    faEgg,
    faFish,
+   faFrown,
    faHamburger,
    faHeart,
    faHeartbeat,
    faHeartBroken,
+   faKissWinkHeart,
+   faLaughBeam,
    faLemon,
-   faMortarPestle,
    faPizzaSlice,
    faPoop,
    faPrescriptionBottleAlt,
    faShower,
    faTint,
+   faToiletPaper,
    faUtensils,
 } from '@fortawesome/free-solid-svg-icons';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import egg from './assets/egg.gif';
 import './PetDot.css';
 
 function PetDot({ extended, onExtend, onCollapse }: IDots): React.ReactElement {
    const [health] = useState(1);
    const [energy] = useState(1);
-   const [love] = useState(0.1);
+   const [love] = useState(1);
    const [hunger] = useState(1);
    const [thirst] = useState(1);
    const [cleanliness] = useState(1);
@@ -50,78 +55,102 @@ function PetDot({ extended, onExtend, onCollapse }: IDots): React.ReactElement {
             <div className="score">
                <div>
                   <FontAwesomeIcon icon={faHeartbeat} />
-                  {getPrecentage(health)}
+                  <span>{getPrecentage(health)}</span>
                </div>
                <div>
                   <FontAwesomeIcon icon={faBolt} />
-                  {getPrecentage(energy)}
+                  <span>{getPrecentage(energy)}</span>
                </div>
                <div>
                   <FontAwesomeIcon icon={love > 0 ? faHeart : faHeartBroken} />
-                  {getPrecentage(love)}
+                  <span>{getPrecentage(love)}</span>
                </div>
                <div>
                   <FontAwesomeIcon icon={faUtensils} />
-                  {getPrecentage(hunger)}
+                  <span>{getPrecentage(hunger)}</span>
                </div>
                <div>
                   <FontAwesomeIcon icon={faTint} />
-                  {getPrecentage(thirst)}
+                  <span>{getPrecentage(thirst)}</span>
                </div>
                <div>
                   <FontAwesomeIcon
                      icon={cleanliness > 0.25 ? faShower : faPoop}
                   />
-                  {getPrecentage(cleanliness)}
+                  <span>{getPrecentage(cleanliness)}</span>
                </div>
             </div>
-            <img src={egg} />
-            <div className="controls">
-               <button className="btn">
-                  <FontAwesomeIcon icon={faFish} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faBacon} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faHamburger} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faPizzaSlice} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faCookie} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faCarrot} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faAppleAlt} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faLemon} />
-               </button>
-               <button className="btn">
-                  <FontAwesomeIcon icon={faTint} />
-               </button>
 
-               <div className="controls">
+            {/* PREVIEW ONLY */}
+            <div className="pet">
+               <img src={egg} />
+            </div>
+
+            <div className="controls">
+               <fieldset
+                  disabled={hunger > 0.8 && thirst > 0.8}
+                  className="category food"
+               >
                   <button className="btn">
-                     <FontAwesomeIcon icon={faHeart} />
+                     <FontAwesomeIcon icon={faFish} />
                   </button>
                   <button className="btn">
-                     <FontAwesomeIcon icon={faHeartBroken} />
+                     <FontAwesomeIcon icon={faBacon} />
                   </button>
                   <button className="btn">
-                     <FontAwesomeIcon icon={faPrescriptionBottleAlt} />
+                     <FontAwesomeIcon icon={faTint} />
+                  </button>
+
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faCarrot} />
                   </button>
                   <button className="btn">
-                     <FontAwesomeIcon icon={faMortarPestle} />
+                     <FontAwesomeIcon icon={faAppleAlt} />
                   </button>
                   <button className="btn">
+                     <FontAwesomeIcon icon={faLemon} />
+                  </button>
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faHamburger} />
+                  </button>
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faPizzaSlice} />
+                  </button>
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faCoffee} />
+                  </button>
+               </fieldset>
+               <fieldset disabled={energy < 0.2} className="category activites">
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faKissWinkHeart} />
+                  </button>
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faLaughBeam} />
+                  </button>
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faFrown} />
+                  </button>
+                  <button className="btn">
+                     <FontAwesomeIcon icon={faAngry} />
+                  </button>
+                  <button className="btn" disabled={cleanliness > 0.8}>
+                     <FontAwesomeIcon icon={faShower} />
+                  </button>
+                  <button className="btn" disabled={cleanliness > 0.8}>
+                     <FontAwesomeIcon icon={faToiletPaper} />
+                  </button>
+               </fieldset>
+               <fieldset className="category wellness">
+                  <button className="btn" disabled={energy > 0.5}>
                      <FontAwesomeIcon icon={faBed} />
                   </button>
-               </div>
+                  <button className="btn" disabled={health === 1}>
+                     <FontAwesomeIcon icon={faPrescriptionBottleAlt} />
+                  </button>
+                  <button className="btn" style={{ color: '#111111' }}>
+                     <FontAwesomeIcon icon={faBox} />
+                  </button>
+               </fieldset>
             </div>
          </div>
       </Dot>
